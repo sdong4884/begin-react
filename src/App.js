f2f4ae4b348da84d2421564c6bb198c8ebd4146d
sdong4884 <sdong4884@gmail.com>
2022-06-26 17:09:49 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,110 +1,122 @@
-import React, { useRef, useState, useMemo, useCallback } from "react";
-import Counter from "./components/Counter";
-import CreateUser from "./components/CreateUser";
-import Hello from './components/Hello';
-import InputSample from "./components/InputSample";
-import InputSample2 from "./components/InputSample2";
-import UserList from "./components/UserList";
-import Wrapper from "./components/Wrapper";
+import React, {
+  useCallback,
+  useMemo,
+  useReducer,
+  useRef,
+  createContext,
+} from 'react'
+import produce from 'immer'
+import CreateUser from './components/CreateUser'
+import UserList from './components/UserList'
+
+window.produce = produce
 
 function countAvticeUsers(users) {
   console.log('활성 사용자 수 세는중 ...')
-  return users.filter(user => user.active).length
+  return users.filter((user) => user.active).length
 }
 
-function App() {
-  const [users, setUsers] = useState([
+const initialState = {
+  inputs: {
+    username: '',
+    email: '',
+  },
+  users: [
     {
       id: 1,
       username: 'dongsong',
       email: 'dongsong@gmail.com',
-      active: true
+      active: true,
     },
     {
       id: 2,
       username: 'tester',
       email: 'tester@gmail.com',
-      active: false
-    }
-  ])
+      active: false,
+    },
+  ],
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      }
+    case 'CREATE_USER':
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      }
+    case 'TOGGLE_USER':
+      // return {
+      //   ...state,
+      //   users: state.users.map(user =>
+      //     user.id === action.id ? { ...user, active: !user.active } : user
+      //   )
+      // }
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id)
+        user.active = !user.active
+      })
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      }
+    default:
+      throw new Error('Unhandled action')
+  }
+}
+
+export const UserDispatch = createContext(null)
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { users } = state
+  const { username, email } = state.inputs
 
   const nextId = useRef(4)
-  const [inputs, setIntpus] = useState({
-    username: '',
-    email: ''
-  })
-  const { username, email } = inputs
 
   const onChange = useCallback((e) => {
     const { name, value } = e.target
-    setIntpus({
-      ...inputs,
-      [name]: value
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value,
     })
-  }, [inputs])
+  }, [])
 
   const onCreate = useCallback(() => {
-    const user = {
-      id: nextId.current,
-      username,
-      email
-    }
-    setUsers(users => [
-      ...users,
-      user
-    ])
-    // setUsers(users.concat(user))
-    setIntpus({
-      username: '',
-      email: ''
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
     })
-    console.log(nextId.current) // 4
     nextId.current += 1
   }, [username, email])
-
-  const onRemove = useCallback((id) => {
-    setUsers(users => users.filter(user => user.id !== id))
-  }, [])
-
-  const onToggle = useCallback((id) => {
-    setUsers(users => users.map(
-      user => user.id === id ? { ...user, active: !user.active } : user
-    ))
-  }, [])
 
   const count = useMemo(() => countAvticeUsers(users), [users])
 
   return (
-    <>
-      <Wrapper>
-        <Hello 
-          name="React" 
-          color="red"
-          isSpecial={true}
-        />
-        <Hello color="blue" />
-
-      </Wrapper>
-
-      <Counter />
-
-      <InputSample />
-      <InputSample2 />
-
-      <CreateUser 
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList 
-        users={users} 
-        onRemove={onRemove}
-        onToggle={onToggle}
-      />
+      <UserList users={users} />
       <div>활성 사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   )
 }
 
-export default App;
+export default App
